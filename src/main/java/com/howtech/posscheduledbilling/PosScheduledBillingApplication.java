d2ab13b1d3ed0f400dacaf0dd5bb5e413a985c18
networkninjadh <networkninjadh@gmail.com>
2022-04-12 12:19:41 -0500

    inititial commit

---
+++ src/main/java/com/howtech/posscheduledbilling/PosScheduledBillingApplication.java
@@ -0,0 +1,13 @@
+package com.howtech.posscheduledbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosScheduledBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosScheduledBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/howtech/posscheduledbilling/PosScheduledBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howtech.posscheduledbilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosScheduledBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
